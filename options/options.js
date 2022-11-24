9c801f0aca5cc596afb52bed253985b24af2b8d1
TAKAHASHI Shuuji <shuuji3@gmail.com>
2022-11-24 19:28:59 +0900

    feat: implement options page to enable to change the link text color

--- options/options.js
+++ options/options.js
@@ -1,79 +1,39 @@
-function onTextEnabledChange() {
-  const textEnabled = document.getElementById("textEnabled").checked;
-  // hide the other text options if text is disabled
-  const elements = document.getElementsByClassName("text-option");
-  for (const elm of elements) {
-    elm.style.display = textEnabled ? "block" : "none";
-  }
-}
-
 function saveOptions() {
-  const memeMode = document.getElementById("memeMode").checked;
-  const textEnabled = document.getElementById("textEnabled").checked;
-  const textVerifiedLabel = document.getElementById("textVerifiedLabel").value;
-  const twitterBlueVerifiedLabel = document.getElementById(
-    "textTwitterBlueLabel"
-  ).value;
-  const textEnableBorder = document.getElementById("textEnableBorder").checked;
-  const removeBlueVerification = document.getElementById(
-    "removeBlueVerification"
-  ).checked;
+  const linkTextColor = document.getElementById("link-text-color").value;
   chrome.storage.local.set(
     {
-      memeMode,
-      textEnabled,
-      removeBlueVerification,
-      textOptions: {
-        verifiedLabel: textEnabled ? textVerifiedLabel : "",
-        twitterBlueLabel: textEnabled ? twitterBlueVerifiedLabel : "",
-        enableBorder: textEnabled ? textEnableBorder : true,
-      },
+      linkTextColor,
     },
     function () {
       const status = document.getElementById("status");
       status.style.display = "block";
       setTimeout(function () {
         status.style.display = "none";
-      }, 1500);
-    }
-  );
-}
-function closeOptions() {
-  window.close();
-}
-function restoreOptions() {
-  chrome.storage.local.get(
-    {
-      memeMode: false,
-      textEnabled: true,
-      removeBlueVerification: false,
-      textOptions: {
-        verifiedLabel: "Verified",
-        twitterBlueLabel: "Paid",
-        enableBorder: true,
-      },
-    },
-    function (items) {
-      document.getElementById("memeMode").checked = items.memeMode;
-      document.getElementById("textEnabled").checked = items.textEnabled;
-      document.getElementById("textVerifiedLabel").value =
-        items.textOptions.verifiedLabel;
-      document.getElementById("textTwitterBlueLabel").value =
-        items.textOptions.twitterBlueLabel;
-      document.getElementById("textEnableBorder").checked =
-        items.textOptions.enableBorder;
-      document.getElementById("removeBlueVerification").checked =
-        items.removeBlueVerification;
-      onTextEnabledChange();
+      }, 5000);
     }
   );
 }
 
+function restoreOptions() {
+  chrome.storage.local.get(
+    {
+      linkTextColor: 'rgb(99, 100, 255)',
+    },
+    function (items) {
+      document.getElementById("link-text-color").value = items.linkTextColor;
+      updateColorPreview()
+    }
+  );
+}
+
+function updateColorPreview() {
+  const colorPreview = document.getElementById("color-preview")
+  const color = document.getElementById("link-text-color").value
+  colorPreview.style.backgroundColor = color;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   restoreOptions();
-  document
-    .getElementById("textEnabled")
-    .addEventListener("change", onTextEnabledChange);
+  document.getElementById("link-text-color").addEventListener("input", updateColorPreview);
   document.getElementById("save").addEventListener("click", saveOptions);
-  document.getElementById("close").addEventListener("click", closeOptions);
 });
